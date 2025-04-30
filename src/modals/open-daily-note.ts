7babdf142d0e5e41cec95d14504c4cf93646620e
Albert O'Shea <albert.oshea@utas.edu.au>
2025-04-30 15:37:33 +1000

    Feature: Open daily note using natural language

---
+++ src/modals/open-daily-note.ts
@@ -0,0 +1,39 @@
+import { Notice, SuggestModal, App } from "obsidian";
+import { getOrCreateDailyNote } from "../utils";
+import type NaturalLanguageDates from "../main";
+import DateSuggest from "../suggest/date-suggest";
+
+// Credit: https://github.com/charliecm/obsidian-open-with-nldates
+export class OpenDailyNoteModal extends SuggestModal<string> {
+  plugin: NaturalLanguageDates;
+
+  constructor(app: App, plugin: NaturalLanguageDates) {
+    super(app);
+    this.plugin = plugin;
+  }
+
+  getSuggestions(query: string): string[] {
+    const tempSuggest = new DateSuggest(this.app, this.plugin);
+    const suggestions = tempSuggest.getDateSuggestions(
+      { query },
+      ['Today', 'Yesterday', 'Tomorrow']
+    );
+    return suggestions.map(s => s.label).length ? suggestions.map(s => s.label) : [query];
+  }
+
+  renderSuggestion(suggestion: string, el: HTMLElement) {
+    el.createEl("div", { text: suggestion });
+  }
+
+  async onChooseSuggestion(suggestion: string) {
+    const parsedDate = this.plugin.parseDate(suggestion);
+    const date = parsedDate.moment;
+    if (!parsedDate.date || !date.isValid()) {
+      new Notice("Unable to parse date");
+      return;
+    }
+
+    const note = await getOrCreateDailyNote(date);
+    this.app.workspace.getLeaf().openFile(note);
+  }
+}
